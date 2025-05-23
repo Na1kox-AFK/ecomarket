0cc4c17d8be53b965c7ddec8c9a4ce37c6565219
Matías <odf>
2025-05-23 16:52:09 -0400

    Actualizacion de service

--- src/main/java/com/example/ecomarket/services/loginServices.java
+++ src/main/java/com/example/ecomarket/services/loginServices.java
@@ -1,6 +1,45 @@
 package com.example.ecomarket.services;
 
-public class loginServices {
+import com.example.ecomarket.repository.LoginRepository;
+import com.example.ecomarket.model.LoginModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
-    private
+@Service
+
+public class loginServices {
+    @Autowired
+    private LoginRepository loginRepository;
+
+    /*listar libros */
+    public List<LoginModel> getLogins(){
+        return loginRepository.obtenerLogin();
+    }
+
+    /*Guardar Login */
+    public LoginModel saveLogin(LoginModel loginModel){
+        return loginRepository.guardar(loginModel);
+    }
+
+    /*Retornar Login */
+    public LoginModel updateLogin(LoginModel loginModel){
+        return loginRepository.actualizar(loginModel);
+    }
+
+    /*retornar Login */
+    public LoginModel getrutModel(String rut){
+        return loginRepository.buscarPorRut(rut);
+    }
+
+    /*Eliminar Login */
+    public String deleteLogin(String rut){
+        loginRepository.Eliminar(rut);
+        return "Login eliminado";
+    }
+
+    public int totalLoginsV1(){
+        return loginRepository.obtenerLogin().size();
+    }
+
 }
