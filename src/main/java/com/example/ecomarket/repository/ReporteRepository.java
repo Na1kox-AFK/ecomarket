7ef8c0d0ae489891c72cecd966eeb3ed39fa5a64
Na1kox_AFK <matias.diaz.reyes2005@gmail.com>
2025-06-12 11:43:51 -0400

    Creation the ReporteRepository

--- src/main/java/com/example/ecomarket/repository/ReporteRepository.java
+++ src/main/java/com/example/ecomarket/repository/ReporteRepository.java
@@ -1,5 +1,19 @@
 package com.example.ecomarket.repository;
 
-public class ReporteRepository {
+import com.example.ecomarket.model.ReportesModel; // Importación del modelo de Reporte
+import org.springframework.data.jpa.repository.JpaRepository; // Importación de JpaRepository para operaciones CRUD
+import org.springframework.stereotype.Repository;
+  
 
+/* Importación de Java */
+import java.util.Optional;
+
+@Repository
+public class ReporteRepository {
+/* Hacemos una extensión para que obtenga todos los métodos del CRUD */
+    public interface ReportesRepository extends JpaRepository<ReportesModel, String> {
+
+        /* Con esta función podemos hacer que la importación Optional busque detalladamente el atributo idReporte */
+        Optional<ReportesModel> findByIdReporte(String idReporte);
+    }
 }
