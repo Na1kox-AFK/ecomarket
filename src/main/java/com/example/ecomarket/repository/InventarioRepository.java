2b7fcba858c373ac3bd6c9bd4a8f4cd6b946326a
Na1kox_AFK <matias.diaz.reyes2005@gmail.com>
2025-07-09 16:43:30 -0400

    Mejora de InventarioRepository

--- src/main/java/com/example/ecomarket/repository/InventarioRepository.java
+++ src/main/java/com/example/ecomarket/repository/InventarioRepository.java
@@ -1,5 +1,13 @@
 package com.example.ecomarket.repository;
 
-public class InventarioRepository {
+import org.springframework.stereotype.Repository;
+import com.example.ecomarket.model.InventarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+@Repository
+public interface InventarioRepository extends JpaRepository<InventarioModel, String> {
+    Optional<InventarioModel> findByIdInventario(String idInventario);
+    
 }
